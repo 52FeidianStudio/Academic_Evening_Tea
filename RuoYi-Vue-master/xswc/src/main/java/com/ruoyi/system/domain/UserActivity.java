65537518dae8fd96fa47805ba869efb0be9af4b4
Sherry <2838916231@qq.com>
2023-11-04 20:28:39 +0800

    个人中心-我的活动功能

--- RuoYi-Vue-master/xswc/src/main/java/com/ruoyi/system/domain/UserActivity.java
+++ RuoYi-Vue-master/xswc/src/main/java/com/ruoyi/system/domain/UserActivity.java
@@ -6,10 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
