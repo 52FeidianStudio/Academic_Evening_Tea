d02265ed64f8682ccb172f5cccf4e738aecd174b
Sherry <2838916231@qq.com>
2023-11-14 15:53:45 +0800

    我要推荐评论新增

--- RuoYi-Vue-master/xswc/src/main/java/com/ruoyi/system/controller/TblRecommendCommnetController.java
+++ RuoYi-Vue-master/xswc/src/main/java/com/ruoyi/system/controller/TblRecommendCommnetController.java
@@ -2,6 +2,8 @@ package com.ruoyi.system.controller;
 
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ruoyi.common.utils.SecurityUtils;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -77,6 +79,8 @@ public class TblRecommendCommnetController extends BaseController
     @PostMapping
     public AjaxResult add(@RequestBody TblRecommendCommnet tblRecommendCommnet)
     {
+        Long userId = SecurityUtils.getUserId();
+        tblRecommendCommnet.setUserId(userId);
         return toAjax(tblRecommendCommnetService.insertTblRecommendCommnet(tblRecommendCommnet));
     }
 
