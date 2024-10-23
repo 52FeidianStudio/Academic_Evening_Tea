219bd278a0f8c3e831a9e745e6321fe79034a2fb
Sherry <2838916231@qq.com>
2024-10-24 00:15:17 +0800

    修改线程池配置

--- RuoYi-Vue-master/ruoyi-framework/src/main/java/com/ruoyi/framework/config/ThreadPoolConfig.java
+++ RuoYi-Vue-master/ruoyi-framework/src/main/java/com/ruoyi/framework/config/ThreadPoolConfig.java
@@ -18,16 +18,16 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class ThreadPoolConfig
 {
     // 核心线程池大小
-    private int corePoolSize = 50;
+    private int corePoolSize = 4;
 
     // 最大可创建的线程数
-    private int maxPoolSize = 200;
+    private int maxPoolSize = 8;
 
     // 队列最大长度
-    private int queueCapacity = 1000;
+    private int queueCapacity = 100;
 
     // 线程池维护线程所允许的空闲时间
-    private int keepAliveSeconds = 300;
+    private int keepAliveSeconds = 120;
 
     @Bean(name = "threadPoolTaskExecutor") //实例化线程池
     public ThreadPoolTaskExecutor threadPoolTaskExecutor()
